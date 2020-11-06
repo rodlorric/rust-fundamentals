9f3b95067387f53fb7c0881b8482787bca4cea39
Ricardo Rodríguez Lorenzo <rrodriguez86@gmail.com>
2020-11-06 18:34:04 +0000

    Building a HTTP Server From Scratch [54..60]

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,12 @@
 #![allow(dead_code)]
 
 use server::Server;
+use website_handler::WebSiteHandler;
+use std::env;
 
 mod http;
 mod server;
+mod website_handler;
 fn main() {
 
     //let string = String::from("127.0.0.0:8080");
@@ -20,9 +23,11 @@ fn main() {
     //let delete = Method::DELETE;
     //let post = Method::POST;
     //let put = Method::PUT;
-
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("public path: {}", public_path);
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run();
+    server.run(WebSiteHandler::new(public_path));
 }
 
 /* GET /user?id=10 HTTP/1.1\r\n
